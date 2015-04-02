cd967ded63f82b2aec0ce950ac986d4678764168
Marcus Widegren <marcuswidegren123@gmail.com>
2015-04-02 12:43:24 +0200

    added some geometry stuff

---
+++ src/main/java/com/marcus/geometry/Sphere.java
@@ -0,0 +1,7 @@
+package com.marcus.geometry;
+
+/**
+ * Created by marcus on 2015-04-02.
+ */
+public class Sphere {
+}
---
+++ src/main/java/com/marcus/math/Constants.java
@@ -0,0 +1,7 @@
+package com.marcus.math;
+
+/**
+ * Created by marcus on 2015-04-02.
+ */
+public class Constants {
+}
